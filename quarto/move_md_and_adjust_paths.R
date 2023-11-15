4c6d7d2995cb0fed1f2a6ad400fe67807c8eff61
LukasGraz <lukas.graz@stat.math.ethz.ch>
2023-11-15 19:37:21 +0100

    .

---
+++ quarto/move_md_and_adjust_paths.R
@@ -0,0 +1,25 @@
+# Author: Lukas Graz             Date: 2023-11-15
+# -----------------------------------------------
+# R file: moves all .md files from $QUARTO_PROJECT_OUTPUT_FILES to ../_posts/ and adjusts the paths in the .md files
+# -----------------------------------------------
+
+# import environment variable
+output.files <- Sys.getenv("QUARTO_PROJECT_OUTPUT_FILES")
+output.files <- strsplit(output.files, "\n")[[1]]
+# only consider the .md files
+md.files  <- output.files[grepl(".md$", output.files)]
+stopifnot(length(md.files) > 0)
+
+print("TEST")
+file <- md.files[1]
+for (file in md.files){
+  fname_without_extension <- gsub("\\..*$", "", basename(file))
+  # rename paths in file
+  xfun::gsub_files(file, 
+    pattern = paste0("\\]\\(", fname_without_extension),
+    replacement = paste0("](../quarto/", dirname(file), "/", fname_without_extension)
+    )
+  # move file
+  file.rename(file, paste0("../_posts/", basename(file)))
+  print("-----------------____-----")
+}
